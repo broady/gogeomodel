54784bab138e2a8d4911047c3d5ff9008adbdc26
Chris Broadfoot <chris@chrisbroadfoot.id.au>
2012-05-25 19:34:32 +1000

    implement geocell encoding/decoding - compatible with python implementation

---
+++ geocell.go
@@ -0,0 +1,63 @@
+package geocell
+
+import (
+	"strings"
+)
+
+const (
+	precision = 13
+	base16    = "0123456789abcdef"
+)
+
+func Encode(lat, lng float64) string {
+	cell := ""
+	lats := [2]float64{-90, 90}
+	lngs := [2]float64{-180, 180}
+
+	for len(cell) < precision {
+		n := 0
+		// interleave bits
+		n |= constrict(&lats, lat) << 3
+		n |= constrict(&lngs, lng) << 2
+		n |= constrict(&lats, lat) << 1
+		n |= constrict(&lngs, lng)
+		cell += string(base16[n])
+	}
+	return cell
+}
+
+func Decode(cell string) ([2]float64, [2]float64) {
+	lats := [2]float64{-90, 90}
+	lngs := [2]float64{-180, 180}
+
+	for _, r := range cell {
+		i := strings.Index(base16, string(r))
+		refine(&lats, (i >> 3)&1)
+		refine(&lngs, (i >> 2)&1)
+		refine(&lats, (i >> 1)&1)
+		refine(&lngs, i&1)
+	}
+	return lats, lngs
+}
+
+func constrict(pair *[2]float64, coord float64) int {
+	m := mid(pair)
+	if coord > m {
+		pair[0] = m
+		return 1
+	}
+	pair[1] = m
+	return 0
+}
+
+func refine(pair *[2]float64, bit int) {
+	if bit != 0 {
+		pair[0] = mid(pair)
+	} else {
+		pair[1] = mid(pair)
+	}
+}
+
+func mid(pair *[2]float64) float64 {
+	return (pair[0] + pair[1]) / 2
+}
---
+++ geocell_test.go
@@ -0,0 +1,37 @@
+package geocell
+
+import "testing"
+
+const (
+	lat  = 37.4
+	lng  = -152.34
+	cell = "8b274e45b9e19"
+)
+
+func Test_Encode_1(t *testing.T) {
+	result := Encode(lat, lng)
+	if result != cell {
+		t.Error(result, cell)
+	}
+}
+func Test_Decode_1(t *testing.T) {
+	lats, lngs := Decode(cell)
+	if lat < lats[0] || lat > lats[1] {
+		t.Error("lat out of range", lat, lats)
+	}
+	if lng < lngs[0] || lng > lngs[1] {
+		t.Error("lng out of range", lng, lngs)
+	}
+}
+
+func Test_Decode_2(t *testing.T) {
+	bad := []byte(cell)
+	bad[3] = 'e'
+	lats, lngs := Decode(string(bad))
+	if lat > lats[0] && lat < lats[1] {
+		t.Error("lat out of range", lat, lats)
+	}
+	if lng > lngs[0] && lng < lngs[1] {
+		t.Error("lng out of range", lng, lngs)
+	}
+}
